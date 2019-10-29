b4004af9935ba418b464f84a5b21ef05dcbfff68
David Ghiassi <david.ghiassi@epita.fr>
2019-10-29 22:46:25 +0100

    updated version.h

--- src/version.h
+++ src/version.h
@@ -4,7 +4,7 @@
     #define VERSION_INSTALL 0
 
     #define VERSION_MAJOR   1
-    #define VERSION_MINOR   0
+    #define VERSION_MINOR   1
     #define VERSION_PATCH   0
 
     #define VERSION_WIN     VERSION_MAJOR,VERSION_MINOR,VERSION_PATCH,0
